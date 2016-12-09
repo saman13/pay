2cb9038f0774392d76375a9588c03ca2c1a99838
mohammadreza.e <mohammadreza.e@mabna.com>
2016-12-09 16:15:30 +0330

    remove all

--- config/db-sample.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;port=8889;dbname=pk;unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock',
-    'username' => 'root',
-    'password' => 'root',
-    'charset' => 'utf8',
-];
